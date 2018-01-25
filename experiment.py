41371b667e781459b52d545a0eafd6f0cf06eea9
Cameron Dykstra <dykstra.cameron@gmail.com>
2018-01-25 15:03:28 +1300

    continue implementing experiment types

--- experiment.py
+++ experiment.py
@@ -1,3 +1,5 @@
+# Author: Cameron Dykstra
+# Email: dykstra.cameron@gmail.com
 
 import os
 import asyncio
@@ -5,7 +7,6 @@ import yaml
 import numpy as np
 import logging
 import importlib
-from abc import ABC, abstractmethod
 
 from program import Program, list_programs
 
@@ -22,7 +23,7 @@ def load_experiment(name: str):
     module = importlib.import_module('experiments.%s' % name)
     return module.Experiment(name)
 
-class BaseExperiment(ABC):
+class BaseExperiment:
     def __init__(self, name: str):
         self.name = name
         self._dir = os.path.join(experiments_dir, name)
@@ -31,8 +32,26 @@ class BaseExperiment(ABC):
         self.programs = {}
         for prog_name in self._config['programs']:
             self.programs[prog_name] = Program(prog_name)
-        super().__init__()
-
-    @abstractmethod
-    def run(self):
+    
+    # must be overridden
+    async def run(self, progress_handler=None):
         pass
+    
+    # may be overridden to add/remove parameters
+    # exposed to the user
+    def get_metadata(self):
+        merged_pars = {}
+        for prog_name, prog in self.programs.items():
+            for par_name, par in prog.config_get('parameters').items():
+                merged_pars[par_name] = par
+        return {'name': self.name,
+                'description': self._config['description'],
+                'parameters': merged_pars}
+    
+    # may be overridden in conjunction with get_metadata() 
+    def set_parameters(self, parameters):
+        for prog_name, prog in self.programs.items():
+            for name, value in parameters.items():
+                if name in prog.config_get('parameters'):
+                    prog.set_par(name, value)
+
--- experiments/CPMG/__init__.py
+++ experiments/CPMG/__init__.py
@@ -7,9 +7,42 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 # All methods have access to the programs object, self.programs
-# which contains the programs listed in config.yaml
+# which contains the pulse programs listed in config.yaml
 # e.g. self.programs['CPMG'] to access the CPMG program
 
 class Experiment(BaseExperiment):
-    def run(self):
-        logger.debug('running %s...' % self.name)
+    # must be async or otherwise return an awaitable
+    async def run(self, progress_handler=None):
+        await self.programs['CPMG'].run(progress_handler=progress_handler)
+    
+    # start a function name with "filter_" for it to be listed as an export format
+    # it must take no arguments and return a JSON serialisable dict
+    def filter_real_imag(self):
+        data = programs['CPMG'].data
+        par = programs['CPMG'].par
+        # deinterleave
+        data = data.astype(np.float32).view(np.complex64)
+        # phase
+        if 'phaseRx' in par:
+            data = data*np.exp(1j*np.pi*par['phaseRx']/180)
+        return {'real': data.real.tolist(), 'imag': data.imag.tolist(), 'unit': 'μV'}
+    
+    # start a function name with "plot_" for it to be listed as a plot type
+    # it must take no arguments and return a JSON serialisable dict
+    def plot_real_imag(self):
+        data = self.filter_real_imag()
+        # return object according to plotly schema
+        return {'data': [{
+                    'name': 'Real',
+                    'type': 'scatter',
+                    'x': list(range(len(data['real']))),
+                    'y': data['real']}, {
+                    'name': 'Imag',
+                    'type': 'scatter',
+                    'x': list(range(len(data['imag']))),
+                    'y': data['imag']}],
+                'layout': {
+                    'title': 'Real/Imaginary data',
+                    'yaxis': {'title': data['unit']}
+                }}
+
