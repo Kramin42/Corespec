304fa1091f4c7cc6b4f0efb1a30ec6a980c4e2a7
Cameron Dykstra <dykstra.cameron@gmail.com>
2018-09-10 12:32:20 +1200

    explicitly define decimation filter

--- program.py
+++ program.py
@@ -339,7 +339,12 @@ class Program:
             self._data_postprocessed = True
             if 'postprocess' in self._config:
                 if 'decimation' in self.config_get('postprocess'):
-                    self._data = signal.decimate(self.data, int(self.config_get('postprocess.decimation')), ftype='iir', zero_phase=True)
+                    dec = int(self.config_get('postprocess.decimation'))
+                    system = signal.dlti(*signal.cheby1(8, 0.05, 0.8 / dec))
+                    b, a = system.num, system.den
+                    self._data = signal.filtfilt(b, a, self._data)
+                    self._data = self._data[slice(None, None, dec)]
+                    #self._data = signal.decimate(self._data, int(self.config_get('postprocess.decimation')), ftype='iir', zero_phase=True)
                     #self._data = np.mean(self._data.reshape(-1, self.config_get('postprocess.decimation')), axis=1)
 
         return np.copy(self._data)  # don't let them change our data!
