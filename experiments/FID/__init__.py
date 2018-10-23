fix integer overflow with many scans.

experiments/FID/__init__.py
@@ -123,7 +123,7 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
 
     def raw_data(self):
         data = self.programs['FID'].data
-        data = data.astype(np.float32).view(np.complex64)
+        data = data.view(np.complex64)
         return data
 
     def autophase(self, data):

experiments/Noise/__init__.py
@@ -89,4 +89,4 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
 
     def raw_data(self):
         data = self.programs['Noise'].data
-        return data.astype(np.float32).view(np.complex64)
+        return data.view(np.complex64)

experiments/T1_CPMG/__init__.py
@@ -32,7 +32,7 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
             logger.debug('running inversion time %s' % inv_time)
             self.programs['InversionRecoveryCPMG'].set_par('inversion_time', inv_time)
             await self.programs['InversionRecoveryCPMG'].run(message_handler=message_handler)
-            run_data = self.programs['InversionRecoveryCPMG'].data.astype(np.float32).view(np.complex64)
+            run_data = self.programs['InversionRecoveryCPMG'].data.view(np.complex64)
             samples = int(self.par['samples'])
             echo_count = int(self.par['echo_count'])
             y = np.zeros(echo_count, dtype=np.complex64)

experiments/T1_FID/__init__.py
@@ -32,7 +32,7 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
             logger.debug('running inversion time %s' % inv_time)
             self.programs['InversionRecoveryFID'].set_par('inversion_time', inv_time)
             await self.programs['InversionRecoveryFID'].run(message_handler=message_handler)
-            run_data = self.programs['InversionRecoveryFID'].data.astype(np.float32).view(np.complex64)
+            run_data = self.programs['InversionRecoveryFID'].data.view(np.complex64)
             if self.data is None:
                 self.data = np.array([run_data])
             else:

experiments/Wobble/__init__.py
@@ -25,7 +25,7 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
         center = self.par['freq']
         samples = int(self.par['samples'])
 
-        y = data.astype(np.float32)
+        y = data
         y = np.mean(y.reshape(-1, samples), axis=1)
         x = np.linspace(center-width/2, center+width/2, len(y))
         return {

hardware/system.py
@@ -105,7 +105,7 @@ def read_dma(
         mem = mmap(f.fileno(), DMA_SIZE, offset=DMA_OFFSET)
         logger.debug('reading from %i to %i' % (offset, offset+length))
         data = np.frombuffer(mem[offset:offset+length], dtype=dtype)
-    return data
+    return data.astype(np.float32)
 
 
 def read_fifo(