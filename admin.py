ace501ee715c868a0f762c9c6cf2c7b8c9dcbc99
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-02-19 09:07:52 +1300

    update version number

--- admin.py
+++ admin.py
@@ -17,7 +17,7 @@ try:
 except:
     logger.warning('Could not run init.sh!')
 
-VERSION = 'flowmeter-1.1.10'
+VERSION = 'flowmeter-1.1.11'
 
 changelog_html = ''
 with open(os.path.join(dir_path, 'changelog.html')) as f:
