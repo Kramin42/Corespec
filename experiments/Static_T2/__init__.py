flowmeter fix gas percent calculation

experiments/Static_T2/__init__.py
@@ -174,9 +174,12 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
             divider_index+=1
         amount_oil = np.sum(S[:divider_index]) / oil_rel_proton_density
         amount_water = np.sum(S[divider_index:])
-        percent_gas = 100*(full_water_initial_amp - data['initial_amp_uV'])/full_water_initial_amp
-        percent_oil = (100 - percent_gas) * (amount_oil) / (amount_water + amount_oil)
-        percent_water = (100 - percent_gas) * (amount_water) / (amount_water + amount_oil)
+        percent_oil = 100 * (amount_oil) / (amount_water + amount_oil)
+        percent_water = 100 * (amount_water) / (amount_water + amount_oil)
+        full_initial_amp = 0.01 * percent_water * full_water_initial_amp + 0.01 * percent_oil * oil_rel_proton_density * full_water_initial_amp
+        percent_gas = 100 * (full_initial_amp - data['initial_amp_uV']) / full_initial_amp
+        percent_water -= 0.01 * percent_gas * percent_water
+        percent_oil -= 0.01 * percent_gas * percent_oil
         return {'data': [{
             'name': '',
             'type': 'scatter',