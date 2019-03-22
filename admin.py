d21a11da7f63023c31e0067dedd1c2060b47ef50
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-03-22 16:59:37 +1300

    update version number

--- admin.py
+++ admin.py
@@ -18,7 +18,7 @@ try:
 except:
     logger.warning('Could not run init.sh!')
 
-VERSION = '1.1.10b'
+VERSION = '1.1.10c'
 
 changelog_html = ''
 with open(os.path.join(dir_path, 'changelog.html')) as f:
