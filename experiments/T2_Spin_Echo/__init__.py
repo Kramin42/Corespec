abbd1a32916d974fac09a12248594daad336820d
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-06-10 17:22:11 +1200

    Implement T2 Spin Echo.

--- experiments/T2_Spin_Echo/__init__.py
+++ experiments/T2_Spin_Echo/__init__.py
@@ -51,8 +51,9 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
     def export_T2(self):
         dwell_time = self.par['dwell_time']/1000000
         sample_shift = self.par['sample_shift']/1000000
+        samples = int(*self.par['samples'])
         fft = np.fft.fft(self.raw_data(), axis=1)
-        freq = np.fft.fftfreq(self.par['samples'], d=dwell_time)[:, np.newaxis]
+        freq = np.fft.fftfreq(samples, d=dwell_time)[:, np.newaxis]
         fft *= np.exp(1j*2*np.pi*-(-0.5*dwell_time*len(fft)+sample_shift)*freq)
         fft *= dwell_time
         halfwidth = int(fft.shape[1]*self.par['int_width']*dwell_time*500)+1
