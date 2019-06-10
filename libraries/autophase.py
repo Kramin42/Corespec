fb6913fbc4f6d79ca0a61b8c2b531df1f0656dc6
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-06-11 11:56:06 +1200

    adjust autophase optimisation function

--- libraries/autophase.py
+++ libraries/autophase.py
@@ -15,8 +15,10 @@ def get_autophase(y, t0=0, dwelltime=1):
 
     def minf(phi):
         fft_y_phased = fft_y * np.exp(1j * phi[0])
-        thresh = (fft_y_phased.real ** 2).max() / 100
-        return np.clip(fft_y_phased.imag ** 2, thresh, None).sum() - np.clip(fft_y_phased.real ** 2, thresh, None).sum()
+        fft_y_phased_R2 = fft_y_phased.real ** 2
+        fft_y_phased_I2 = fft_y_phased.imag ** 2
+        thresh = max(fft_y_phased_R2.max(), fft_y_phased_I2.max()) / 100
+        return np.clip(fft_y_phased_I2, thresh, None).sum() - np.clip(fft_y_phased_R2, thresh, None).sum()
 
     res = opt.minimize(minf, np.array([0]), method='nelder-mead')
 
