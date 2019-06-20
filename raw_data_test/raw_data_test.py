772f34dd5125af210c7b8c45a1e28f749caac3ac
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-06-20 01:46:51 +0000

    fix raw data test script

--- raw_data_test/raw_data_test.py
+++ raw_data_test/raw_data_test.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3.6
 
-samples = 10000
+samples = 64000
 
 import numpy as np
 from hardware import system
 
 system.stop()
 system.write_elf('programs/Raw/RAW_DMA.elf')
-system.write_par(0xc4, samples/4)
+system.write_par(0xc4, samples/2)
 system.write_par(0x0, 1)
 system.run()
 while system.read_par(0x4)!=10:
     pass
 
-data = system.read_dma(0, samples, "uint16")
-print('data (interpreted as uint16):', data)
+data = system.read_dma(0, samples, "int16")
+print('data:', data)
 
 for i in range(16):
     mask = (i+1)**2
     count = np.sum(np.bitwise_and(data, mask)/mask)
     print('bit %i count: %d' % (i, count))
+
+np.save('data', data)
