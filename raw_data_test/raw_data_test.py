da6e002af5a286b3dbbdade7b3c80e40e1f66d1b
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-06-20 03:28:36 +0000

    add raw_data_test notes

--- raw_data_test/raw_data_test.py
+++ raw_data_test/raw_data_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 
-samples = 64000
+samples = 256000
 
 import numpy as np
 from hardware import system
@@ -14,11 +14,17 @@ while system.read_par(0x4)!=10:
     pass
 
 data = system.read_dma(0, samples, "int16")
-print('data:', data)
+print('int16 range: -32767 to +32768')
+print('data[:10]:', data[:10])
+
+data_float = data.astype(float)
+
+print('data max:', data.max(), ', min:', data.min(), ', p-p:', data.max()-data.min())
+print('data mean:', data_float.mean(), ', RMS:', np.sqrt(np.mean(data_float*data_float)))
 
 for i in range(16):
     mask = (i+1)**2
     count = np.sum(np.bitwise_and(data, mask)/mask)
     print('bit %i count: %d' % (i, count))
 
-np.save('data', data)
+np.save('adc_raw_data', data)
