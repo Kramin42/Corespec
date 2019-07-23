4eef72bc2a6af1c895880258d55daea76c067f5d
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-07-24 11:01:40 +1200

    use better autophase in shimming experiment

--- experiments/Shimming/__init__.py
+++ experiments/Shimming/__init__.py
@@ -1,4 +1,5 @@
 from experiment import BaseExperiment # required
+from libraries.autophase import get_autophase
 
 # for debugging
 import logging
@@ -138,8 +139,8 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
         return data
 
     def autophase(self, data):
-        phase = np.angle(np.sum(data)) # get average phase
-        return data * np.exp(1j * -phase) # rotate
+        phase = get_autophase(data)
+        return data * np.exp(1j * phase)  # rotate
 
     def gaussian_apodize(self, data, lb):
         t = np.linspace(0, 1, len(data))
