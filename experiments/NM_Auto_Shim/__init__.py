4213defa4eb54fd54b473583585398da0698cc39
Cameron Dykstra <dykstra.cameron@gmail.com>
2019-09-13 16:18:55 +1200

    test nelder-mead autoshim

---
+++ experiments/NM_Auto_Shim/__init__.py
@@ -0,0 +1,183 @@
+from experiment import BaseExperiment # required
+from libraries.autophase import get_autophase
+from libraries.optimisation import nelder_mead_async
+
+# for debugging
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+import numpy as np
+
+# All methods have access to the programs object, self.programs
+# which contains the pulse programs listed in config.yaml
+# e.g. self.programs['CPMG'] to access the CPMG program
+
+class Experiment(BaseExperiment): # must be named 'Experiment'
+    # must be async or otherwise return an awaitable
+    async def run(self, progress_handler=None, message_handler=None):
+        self.data = None
+        self.shims = np.array([int(self.par['shim_X']),
+                               int(self.par['shim_Y']),
+                               int(self.par['shim_Z']),
+                               int(self.par['shim_Z2']),
+                               int(self.par['shim_ZX']),
+                               int(self.par['shim_ZY']),
+                               int(self.par['shim_XY']),
+                               int(self.par['shim_X2Y2'])])
+
+        O1_shim_step = int(self.par['O1_shim_step'])
+        O2_shim_step = int(self.par['O2_shim_step'])
+        init_step = [O1_shim_step,O1_shim_step,O1_shim_step,O2_shim_step,O2_shim_step,O2_shim_step,O2_shim_step,O2_shim_step]
+        iterations = int(self.par['shim_iterations'])
+        if progress_handler is not None:
+            progress_handler(0, iterations)
+        async def evalfunc(try_shims):
+            message_handler('Trying (X: %d, Y: %d, Z: %d, Z2: %d, ZX: %d, ZY: %d, XY: %d, X2Y2: %d)' %
+                            tuple(try_shims.tolist()))
+            self.programs['FID'].set_par('shim_X', try_shims[0])
+            self.programs['FID'].set_par('shim_Y', try_shims[1])
+            self.programs['FID'].set_par('shim_Z', try_shims[2])
+            self.programs['FID'].set_par('shim_Z2', try_shims[3])
+            self.programs['FID'].set_par('shim_ZX', try_shims[4])
+            self.programs['FID'].set_par('shim_ZY', try_shims[5])
+            self.programs['FID'].set_par('shim_XY', try_shims[6])
+            self.programs['FID'].set_par('shim_X2Y2', try_shims[7])
+            await self.programs['FID'].run(progress_handler=None,
+                                           message_handler=message_handler)
+            self.data = self.programs['FID'].data
+            y = self.gaussian_apodize(self.data, self.par['gaussian_lb'])
+            result = np.sum(np.abs(y) ** 2)
+            message_handler('SumSq: %d' % result)
+            return -result
+        opt_res = await nelder_mead_async(evalfunc, self.shims, step=init_step, message_handler=message_handler, progress_handler=progress_handler)
+        message_handler('Final Shims: (X: %d, Y: %d, Z: %d, Z2: %d, ZX: %d, ZY: %d, XY: %d, X2Y2: %d)' %
+                        tuple(opt_res[0].tolist()))
+    
+    # start a function name with "export_" for it to be listed as an export format
+    # it must take no arguments and return a JSON serialisable dict
+    def export_Raw(self):
+        y = self.autophase(self.raw_data())
+        y = self.gaussian_apodize(y, self.par['gaussian_lb'])
+        x = np.linspace(0, self.par['dwell_time']*len(y), len(y), endpoint=False)
+        y /= 1000000  # μV->V
+        x /= 1000000  # μs->s
+        return {
+            'x': x,
+            'y_real': y.real,
+            'y_imag': y.imag,
+            'y_mag': np.abs(y),
+            'y_unit': 'V',
+            'x_unit': 's'}
+
+
+    def export_FT(self):
+        y = self.autophase(self.raw_data())
+        y = self.gaussian_apodize(y, self.par['gaussian_lb'])
+        dwell_time = self.par['dwell_time']*0.000001  # μs->s
+        fft = np.fft.fft(y)
+        freq = np.fft.fftfreq(y.size, d=dwell_time)
+        # sort the frequency axis
+        p = freq.argsort()
+        freq = freq[p]
+        fft = fft[p]
+        fft /= 1000000  # μV->V
+        fft *= dwell_time*1000  # V->V/kHz
+        return {
+            'freq': freq,
+            'fft_real': fft.real,
+            'fft_imag': fft.imag,
+            'fft_mag': np.abs(fft),
+            'fft_unit': 'V/kHz',
+            'freq_unit': 'Hz'}
+    
+    # start a function name with "plot_" for it to be listed as a plot type
+    # it must take no arguments and return a JSON serialisable dict
+    def plot_Raw(self):
+        data = self.export_Raw()
+        # return object according to plotly schema
+        return {'data': [{
+                    'name': 'Real',
+                    'type': 'scatter',
+                    'x': data['x'],
+                    'y': data['y_real']}, {
+                    'name': 'Imag.',
+                    'type': 'scatter',
+                    'x': data['x'],
+                    'y': data['y_imag']}, {
+                    'name': 'Mag.',
+                    'type': 'scatter',
+                    'x': data['x'],
+                    'y': data['y_mag']}],
+                'layout': {
+                    'title': 'Real/Imaginary data',
+                    'xaxis': {'title': data['x_unit']},
+                    'yaxis': {'title': data['y_unit']}
+                }}
+
+    def plot_Phase(self):
+        y = self.autophase(self.raw_data())
+        x = np.linspace(0, self.par['dwell_time']*len(y), len(y), endpoint=False)
+        x /= 1000000  # μs->s
+        
+        # return object according to plotly schema
+        return {'data': [{
+                    'name': '',
+                    'type': 'scatter',
+                    'x': x,
+                    'y': np.angle(y)}],
+                'layout': {
+                    'title': 'Phase',
+                    'xaxis': {'title': 's'},
+                    'yaxis': {'title': 'rad'}
+                }}
+
+    def plot_FT(self):
+        data = self.export_FT()
+        peak_index = np.argmax(data['fft_mag'])
+        peak_freq_offset = data['freq'][peak_index]/1000000  # in MHz
+        avg_start = peak_index - int(len(data['freq'])/20)
+        avg_end = peak_index + int(len(data['freq'])/20) + 1
+        if avg_start>0 and avg_end<=len(data['freq']):
+            peak_freq_offset = np.average(data['freq'][avg_start:avg_end], weights=np.square(data['fft_mag'][avg_start:avg_end]))/1000000  # in MHz
+        peak_freq = self.par['freq'] + peak_freq_offset
+        height = data['fft_real'].max()
+        left_hw_index = -1
+        right_hw_index = -1
+        for i, x in enumerate(data['fft_real']):
+            if x > height/2:
+                if left_hw_index==-1:
+                    left_hw_index = i
+                right_hw_index = i
+        half_width = data['freq'][right_hw_index] - data['freq'][left_hw_index]
+        return {'data': [{
+            'name': 'Real',
+            'type': 'scatter',
+            'x': data['freq'],
+            'y': data['fft_real']}, {
+            'name': 'Imag.',
+            'type': 'scatter',
+            'x': data['freq'],
+            'y': data['fft_imag']}, {
+            'name': 'Mag.',
+            'type': 'scatter',
+            'x': data['freq'],
+            'y': data['fft_mag']}],
+            'layout': {
+                'title': 'FFT (peak@{:0.4f}{}, height: {:0.4f} uV, 50% width: {:.0f} Hz)'.format(peak_freq, 'M'+data['freq_unit'], height*1000000, half_width),
+                'xaxis': {'title': data['freq_unit']},
+                'yaxis': {'title': data['fft_unit']}
+            }}
+
+    def raw_data(self):
+        if self.data is None:
+            raise Exception('Data is not ready to be read!')
+        return self.data
+
+    def autophase(self, data):
+        phase = get_autophase(data)
+        return data * np.exp(1j * phase)  # rotate
+
+    def gaussian_apodize(self, data, lb):
+        t = np.linspace(0, 1, len(data))
+        return data * np.exp(-lb*lb*t*t)
---
+++ libraries/optimisation.py
@@ -0,0 +1,220 @@
+import copy
+import numpy as np
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def nelder_mead(f, x_start,
+                step=1, no_improve_thr=10e-6,
+                no_improv_break=10, max_iter=0,
+                alpha=1., gamma=2., rho=0.5, sigma=0.5):
+    '''
+        @param f (function): function to optimize, must return a scalar score
+            and operate over a numpy array of the same dimensions as x_start
+        @param x_start (numpy array): initial position
+        @param step (float): look-around radius in initial step
+        @no_improv_thr,  no_improv_break (float, int): break after no_improv_break iterations with
+            an improvement lower than no_improv_thr
+        @max_iter (int): always break after this number of iterations.
+            Set it to 0 to loop indefinitely.
+        @alpha, gamma, rho, sigma (floats): parameters of the algorithm
+            (see Wikipedia page for reference)
+        return: tuple (best parameter array, best score)
+    '''
+
+    # init
+    n = len(x_start)
+    prev_best = f(x_start)
+    no_improv = 0
+    res = [[x_start, prev_best]]
+
+    for i in range(n):
+        x = np.copy(x_start)
+        if isinstance(step, (list, np.ndarray)):
+            x[i] = x[i] + step[i]
+        else:
+            x[i] = x[i] + step
+        score = f(x)
+        res.append([x, score])
+
+    print(res)
+
+    # simplex iter
+    iters = 0
+    while 1:
+        print('iter', iters)
+        # order
+        res.sort(key=lambda x: x[1])
+        best = res[0][1]
+
+        # break after max_iter
+        if max_iter > 0 and iters >= max_iter:
+            return res[0]
+        iters += 1
+
+        # break after no_improv_break iterations with no improvement
+        print('...best so far: %f' % best)
+
+        if best < prev_best - no_improve_thr*np.abs(prev_best):
+            no_improv = 0
+            prev_best = best
+        else:
+            no_improv += 1
+
+        if no_improv >= no_improv_break:
+            return res[0]
+
+        # centroid
+        x0 = np.zeros(n)
+        for tup in res[:-1]:
+            x0 += tup[0] / n
+
+        print('centroid', x0)
+
+        # reflection
+        xr = x0 + alpha*(x0 - res[-1][0])
+        rscore = f(xr)
+        if res[0][1] <= rscore < res[-2][1]:
+            del res[-1]
+            res.append([xr, rscore])
+            continue
+
+        # expansion
+        if rscore < res[0][1]:
+            xe = x0 + gamma*(xr - x0)
+            escore = f(xe)
+            if escore < rscore:
+                del res[-1]
+                res.append([xe, escore])
+                continue
+            else:
+                del res[-1]
+                res.append([xr, rscore])
+                continue
+
+        # contraction
+        xc = x0 + rho*(res[-1][0] - x0)
+        cscore = f(xc)
+        if cscore < res[-1][1]:
+            del res[-1]
+            res.append([xc, cscore])
+            continue
+
+        # reduction
+        x1 = res[0][0]
+        nres = []
+        for tup in res:
+            redx = x1 + sigma*(tup[0] - x1)
+            score = f(redx)
+            nres.append([redx, score])
+        res = nres
+
+async def nelder_mead_async(f, x_start,
+                step=1, no_improve_thr=10e-6,
+                no_improv_break=10, max_iter=0,
+                alpha=1., gamma=2., rho=0.5, sigma=0.5,
+                progress_handler=None, message_handler=None):
+    '''
+        @param f (function): function to optimize, must return a scalar score
+            and operate over a numpy array of the same dimensions as x_start
+        @param x_start (numpy array): initial position
+        @param step (float): look-around radius in initial step
+        @no_improv_thr,  no_improv_break (float, int): break after no_improv_break iterations with
+            an improvement lower than no_improv_thr
+        @max_iter (int): always break after this number of iterations.
+            Set it to 0 to loop indefinitely.
+        @alpha, gamma, rho, sigma (floats): parameters of the algorithm
+            (see Wikipedia page for reference)
+        return: tuple (best parameter array, best score)
+    '''
+
+    # init
+    n = len(x_start)
+    prev_best = await f(x_start)
+    no_improv = 0
+    res = [[x_start, prev_best]]
+
+    for i in range(n):
+        x = np.copy(x_start)
+        if isinstance(step, (list, np.ndarray)):
+            x[i] = x[i] + step[i]
+        else:
+            x[i] = x[i] + step
+        score = await f(x)
+        res.append([x, score])
+
+    print(res)
+
+    # simplex iter
+    iters = 0
+    while 1:
+        print('iter', iters)
+        # order
+        res.sort(key=lambda x: x[1])
+        best = res[0][1]
+
+        # break after max_iter
+        if max_iter > 0 and iters >= max_iter:
+            return res[0]
+        iters += 1
+
+        if progress_handler is not None:
+            progress_handler(iters, max_iter)
+
+        # break after no_improv_break iterations with no improvement
+        print('...best so far: %f' % best)
+
+        if best < prev_best - no_improve_thr*np.abs(prev_best):
+            no_improv = 0
+            prev_best = best
+        else:
+            no_improv += 1
+
+        if no_improv >= no_improv_break:
+            return res[0]
+
+        # centroid
+        x0 = np.zeros(n)
+        for tup in res[:-1]:
+            x0 += tup[0] / n
+
+        print('centroid', x0)
+
+        # reflection
+        xr = x0 + alpha*(x0 - res[-1][0])
+        rscore = await f(xr)
+        if res[0][1] <= rscore < res[-2][1]:
+            del res[-1]
+            res.append([xr, rscore])
+            continue
+
+        # expansion
+        if rscore < res[0][1]:
+            xe = x0 + gamma*(xr - x0)
+            escore = await f(xe)
+            if escore < rscore:
+                del res[-1]
+                res.append([xe, escore])
+                continue
+            else:
+                del res[-1]
+                res.append([xr, rscore])
+                continue
+
+        # contraction
+        xc = x0 + rho*(res[-1][0] - x0)
+        cscore = await f(xc)
+        if cscore < res[-1][1]:
+            del res[-1]
+            res.append([xc, cscore])
+            continue
+
+        # reduction
+        x1 = res[0][0]
+        nres = []
+        for tup in res:
+            redx = x1 + sigma*(tup[0] - x1)
+            score = await f(redx)
+            nres.append([redx, score])
+        res = nres
