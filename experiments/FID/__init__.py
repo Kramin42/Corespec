fix FID frequency calculation.

experiments/FID/__init__.py
@@ -96,7 +96,7 @@ class Experiment(BaseExperiment): # must be named 'Experiment'
         peak_index = np.searchsorted(fft_abs_sumsq, fft_abs_sumsq[-1] / 2.0)
         # interpolate
         peak_index_interp = (fft_abs_sumsq[-1] / 2.0 - fft_abs_sumsq[peak_index - 1]) / (fft_abs_sumsq[peak_index] - fft_abs_sumsq[peak_index - 1]) - 1
-        peak_freq = self.par['freq'] + 1e-6*data['freq'][peak_index] + peak_index_interp/self.par['dwell_time']
+        peak_freq = self.par['freq'] + 1e-6*(data['freq'][peak_index] + peak_index_interp*(data['freq'][1]-data['freq'][0]))
         return {'data': [{
             'name': 'Real',
             'type': 'scatter',