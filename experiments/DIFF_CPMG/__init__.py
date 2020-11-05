cde87a2264cee6da8a736bd033230dad8cc07c00
Cameron Dykstra <cameron@resonint.com>
2020-11-05 15:07:57 +1300

    Modify DIFF_CPMG eperiment to run a range of echo times, producing 2D data.

--- experiments/DIFF_CPMG/__init__.py
+++ experiments/DIFF_CPMG/__init__.py
@@ -1,6 +1,5 @@
-from experiment import BaseExperiment # required
-from libraries.invlaplace import getT2Spectrum
-from libraries.autophase import get_autophase
+from experiment import BaseExperiment  # required
+from libraries.expfitting import fit_single_exp
 
 # for debugging
 import logging
@@ -14,185 +13,98 @@ import numpy as np
 # e.g. self.programs['CPMG'] to access the CPMG program
 
 class Experiment(BaseExperiment): # must be named 'Experiment'
+    def override(self):
+        del self.par_def['echo_time']
+
     # must be async or otherwise return an awaitable
     async def run(self, progress_handler=None, message_handler=None):
         await self.programs['DIFF_CPMG'].run(progress_handler=progress_handler,
                                         message_handler=message_handler)
-        y = self.autophase(self.integrated_data())
-        if y.size >= 10:
-            SNR = np.mean(y.real[:2]).item() / np.sqrt(np.mean(y.imag[int(y.size/2):] * y.imag[int(y.size/2):])).item()
-            message_handler('SNR estimate: %d' % SNR)
 
-    # start a function name with "export_" for it to be listed as an export format
-    # it must take no arguments and return a JSON serialisable dict
+        self.echo_times = np.round(np.logspace(
+            np.log10(self.par['TE_start']),
+            np.log10(self.par['TE_end']),
+            int(self.par['TE_steps']),
+            endpoint=True)).astype(np.int)
+        self.data = None
+        self.T2s = []
+        self.last_index = 0
+        for i, TE in enumerate(self.echo_times):
+            if progress_handler is not None:
+                progress_handler(i, len(self.echo_times))
+            self.programs['DIFF_CPMG'].set_par('echo_time', TE)
+            await self.programs['DIFF_CPMG'].run(message_handler=message_handler)
+            run_data = self.programs['DIFF_CPMG'].data.view(np.complex64)
+            samples = int(self.par['samples'])
+            echo_count = int(self.par['echo_count'])
+            y = np.mean(np.reshape(run_data, (echo_count, samples)), axis=1)
+            x = np.linspace(0, TE * len(y), len(y), endpoint=False)
+            popt, stderr = fit_single_exp(x, y)
+            self.T2s.append(1/popt[1])
+            if self.data is None:
+                self.data = np.array([y])
+            else:
+                self.data = np.append(self.data, [y], axis=0)
+            self.last_index = i
+        if progress_handler is not None:
+            progress_handler(len(self.echo_times), len(self.echo_times))
+
     def export_Raw(self):
-        samples = int(self.par['samples'])
-        echo_count = int(self.par['echo_count'])
-        dwell_time = self.par['dwell_time']
-        data = self.autophase(self.raw_data())
-        t = np.zeros(data.size)
-        offset = 0
-        sample_times = np.linspace(0, samples*dwell_time,
-                                 num=samples, endpoint=False)
-        for i in range(echo_count):
-            t[i*samples:(i+1)*samples] = sample_times + offset
-            offset += self.par['echo_time']
-        t/= 1000000  # μs -> s
-        data/=1000000  # μV -> V
+        phase = np.angle(np.sum(self.raw_data()[0, :]))
+        data = self.raw_data() * np.exp(1j * -phase)
+        data /= 1000000  # uV -> V
+        TE_axis = np.array(self.echo_times)/1000000  # us -> s
+        NE_axis = np.array(range(data.shape[1]))
         return {
-            'time': t,
-            'real': data.real,
-            'imag': data.imag,
-            'unit': 'V',
-            'time_unit': 's'}
-    
-    def export_Echo_Integrals(self, decimation=1):
-        y = self.autophase(self.integrated_data(decimation=decimation))
-        y/=1000000 # μV -> V
-        echo_count = int(self.par['echo_count']/decimation)
-        echo_time = decimation*self.par['echo_time']/1000000.0
-        x = np.linspace(0, echo_count*echo_time, echo_count, endpoint=False)
-        return {
-            'x': x,
-            'y_real': y.real,
-            'y_imag': y.imag,
-            'y_mag': np.absolute(y),
-            'x_unit': 's',
-            'y_unit': 'V'}
-
-    def export_Echo_Envelope(self):
-        data = self.autophase(self.raw_data())
-        samples = int(self.par['samples'])
-        echo_count = int(self.par['echo_count'])
-        dwell_time = self.par['dwell_time']
-        dwell_time/=1000000 # μs -> s
-        x = np.linspace(0, dwell_time*samples, samples, endpoint=False)
-        # y = np.zeros(samples, dtype=np.complex64)
-        y = data.reshape((echo_count, samples)).mean(axis=0)
-        # for i in range(len(data)):
-        #     y[i%samples] += data[i]
-        # y /= echo_count
-        y /= 1000000  # μV -> V
-        return {
-            'x': x,
-            'y_real': y.real,
-            'y_imag': y.imag,
-            'x_unit': 's',
-            'y_unit': 'V'}
-
-    def export_T2_Spectrum(self):
-        echo_count = int(self.par['echo_count'])
-        echo_time = self.par['echo_time'] / 1000000.0  # μs -> s
-        Y = self.autophase(self.integrated_data())
-        t = np.linspace(0, echo_count*echo_time, echo_count, endpoint=False)
-        T2 = np.logspace(-5, 2, 200+1, endpoint=True)  # TODO: add parameters for T2 range/points
-        S = getT2Spectrum(t, Y.real, Y.imag, T2, fixed_alpha=1)  # TODO: determine alpha automatically
-        return {
-            'x': T2,
-            'y': S,
-            'x_unit': 's',
-            'y_unit': 'arb. units.',
-            'initial_amp_uV': np.mean(Y[0:4].real)
+            'echo_time_axis': TE_axis[:, np.newaxis],
+            'echo_number_axis': NE_axis[np.newaxis, :],
+            'y_real': data.real,
+            'y_imag': data.imag,
+            'y_units': 'V',
+            'echo_time_units': 's'
         }
-
-    # def export_default(self):
-    #     export = self.export_Raw()
-    #     export_spectrum = self.export_T2_Spectrum()
-    #     export['spectrum_T2'] = export_spectrum['x']
-    #     export['spectrum'] = export_spectrum['y']
-    #     export['spectrum_T2_unit'] = export_spectrum['x_unit']
-    #     export['spectrum_unit'] = export_spectrum['y_unit']
-    #     return export
-
-    # start a function name with "plot_" for it to be listed as a plot type
-    # it must take no arguments and return a JSON serialisable dict
-    def plot_Raw(self):
-        data = self.export_Raw()
-        # return object according to plotly schema
-        return {'data': [{
-                    'name': 'Real',
-                    'type': 'scatter',
-                    'x': data['time'],
-                    'y': data['real']}, {
-                    'name': 'Imag',
-                    'type': 'scatter',
-                    'x': data['time'],
-                    'y': data['imag']}],
-                'layout': {
-                    'title': 'Raw data',
-                    'yaxis': {'title': data['unit']},
-                    'xaxis': {'title': data['time_unit']}
-                }}
     
-    def plot_Echo_Integrals(self):
-        # with very large data, decimate before sending plots
-        if self.par['echo_count'] > 10000:
-            data = self.export_Echo_Integrals(decimation=int(self.par['echo_count']/10000))
-        else:
-            data = self.export_Echo_Integrals()
+    def plot_CPMG(self):
+        y = self.autophase(self.raw_data()[self.last_index, :])
+        x = np.linspace(0, self.echo_times[self.last_index] * len(y), len(y), endpoint=False)
+        y /= 1000000  # μV->V
+        x /= 1000000  # μs->s
         return {'data': [{
-                    'name': 'Real',
-                    'type': 'scatter',
-                    'x': data['x'],
-                    'y': data['y_real']}, {
-                    'name': 'Imaginary',
-                    'type': 'scatter',
-                    'x': data['x'],
-                    'y': data['y_imag']}],
-                #    'name': 'Mag',
-                #    'type': 'scatter',
-                #    'x': data['x'],
-                #    'y': data['y_mag']}],
-                'layout': {
-                    'title': 'Echo Integrals',
-                    'xaxis': {'title': data['x_unit']},
-                    'yaxis': {'title': data['y_unit']}
-                }}
-
-    def plot_Echo_Envelope(self):
-        data = self.export_Echo_Envelope()
-        return {'data': [{
-                    'name': 'Real',
-                    'type': 'scatter',
-                    'x': data['x'],
-                    'y': data['y_real']}, {
-                    'name': 'Imag',
-                    'type': 'scatter',
-                    'x': data['x'],
-                    'y': data['y_imag']}],
-                'layout': {
-                    'title': 'Average Echo Envelope',
-                    'xaxis': {'title': data['x_unit']},
-                    'yaxis': {'title': data['y_unit']}
-                }}
-
-    def plot_T2_Spectrum(self):
-        data = self.export_T2_Spectrum()
-        return {'data': [{
-            'name': '',
+            'name': 'Real',
             'type': 'scatter',
-            'x': np.log10(data['x']),
-            'y': data['y']}],
-        'layout': {
-            'title': 'T2 Spectrum',
-            'xaxis': {'title': 'log10(T2) (%s)' % data['x_unit']},
-            'yaxis': {'title': 'Incremental Volume (%s)' % data['y_unit']}
-        }}
-    
-    def raw_data(self):
-        data = self.programs['DIFF_CPMG'].data
-        # deinterleave
-        data = data.view(np.complex64)
-        return data
+            'x': x,
+            'y': y.real}, {
+            'name': 'Imag',
+            'type': 'scatter',
+            'x': x,
+            'y': y.imag}],
+            'layout': {
+                'title': 'CPMG Echo Integrals',
+                'xaxis': {'title': 's'},
+                'yaxis': {'title': 'V'}
+            }}
 
-    def integrated_data(self, decimation=1):
-        data = self.raw_data()
-        samples = int(self.par['samples'])
-        echo_count = int(self.par['echo_count']/decimation)
-        y = np.zeros(echo_count, dtype=np.complex64)
-        for i in range(echo_count):
-            y[i] = np.mean(data[i * decimation * samples:(i*decimation + 1) * samples])
-        return y
+    def plot_T2(self):
+        y = self.T2s
+        x = np.array(self.echo_times[:len(y)])/1000000  # us -> s
+        result = {'data': [{
+            'name': 'T2',
+            'type': 'scatter',
+            'x': x,
+            'y': y
+        }],
+            'layout': {
+                'title': 'T2 vs TE',
+                'xaxis': {'title': 'TE (s)'},
+                'yaxis': {'title': 'T2 (s)'}
+            }}
+        return result
+
+
+    def raw_data(self):
+        if self.data is None:
+            raise Exception('Data not ready!')
+        return self.data
 
     def autophase(self, data):
         phase = -np.angle(np.sum(data))  # use simple method for CPMG
---
+++ libraries/expfitting.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def single_exp(x, a, b):
+    return a * np.exp(-b * x)
+
+
+def multi_exp(x, *args):
+    return np.sum([a * np.exp(-b * x) for a, b in zip(args[::2], args[1::2])], axis=0)
+
+
+def fit_single_exp(x, y, weights=None, T2_min=10**-4, T2_max=10**2):
+    sigma = None
+    if weights is not None:
+        sigma = 1 / weights
+    spopt, spcov = curve_fit(single_exp, x, y, sigma=sigma, p0=[0, 1], bounds=([0, 1 / T2_max], [np.inf, 1 / T2_min]))
+    y_fit = single_exp(x, *spopt)
+    stderr = np.sqrt(np.sum((y_fit - y) ** 2) / len(y))
+    return spopt, stderr
+
+
+def fit_multi_exp(x, y, weights=None, N_exp=2, T2_min=10**-4, T2_max=10**2):
+    sigma = None
+    if weights is not None:
+        sigma = 1 / weights
+    spopt, spcov = curve_fit(single_exp, x, y, sigma=sigma, p0=[0, 1], bounds=([0, 1/T2_max], [np.inf, 1/T2_min]))
+    mp0 = [0, spopt[1]] * N_exp
+    mp0[0] = spopt[0]
+    # set up initial guesses for decay parameter
+    mp0[1::2] = [spopt[1] * (2 ** (i)) for i in range(N_exp)]
+    mp0[1::2] = np.clip(mp0[1::2], 1 / T2_max, 1 / T2_min)
+    mpopt, mpcov = curve_fit(multi_exp, x, y, sigma=sigma, p0=mp0, bounds=([0, 1/T2_max]*N_exp, [np.inf, 1/T2_min]*N_exp))
+    y_fit = multi_exp(x, *mpopt)
+    mstderr = np.sqrt(np.sum((y_fit - y) ** 2) / len(y))
+    return mpopt, mstderr
